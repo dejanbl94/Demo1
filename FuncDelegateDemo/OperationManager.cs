108e0e78a04f30db9b8217c97749bb993abf7f5d
Dejan Tamamovic <dejanbltamamovic@gmail.com>
2021-01-30 21:17:52 +0100

    prvi

---
+++ FuncDelegateDemo/OperationManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FuncDelegateDemo
+{
+    public class OperationManager
+    {
+        private int firstOperand;
+        private int secondOperand;
+        private ExecutionManager executionManager;
+        public event Action OnChangeDivide;
+
+        public OperationManager(int firstOperand, int secondOperand, ExecutionManager execManager)
+        {
+            this.firstOperand = firstOperand;
+            this.secondOperand = secondOperand;
+            executionManager = execManager;
+
+            execManager.PopulateFunc(this.Add, this.Subtract, this.Multiply, this.Divide, this.Modulo);
+            execManager.PrepareFunc();
+        }
+
+        public int Execute(Operation operation)
+        {
+            return executionManager.FuncExec.ContainsKey(operation) ?
+                                                executionManager.FuncExec[operation]() : -1;
+        }
+
+        private int Add()
+        {
+            return firstOperand + secondOperand;
+        }
+
+        private int Subtract()
+        {
+            return firstOperand > secondOperand ? firstOperand - secondOperand : secondOperand - firstOperand;
+        }
+
+        private int Multiply()
+        {
+            return firstOperand * secondOperand;
+        }
+
+        private int Divide()
+        {
+            if (secondOperand == 0)
+            {
+                OnChangeDivide?.Invoke();
+            }
+                return secondOperand != 0 ? firstOperand / secondOperand : -1;
+            
+        }
+
+        private int Modulo()
+        {
+            return firstOperand % secondOperand;
+        }
+    }
+}
---
+++ FuncDelegateDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FuncDelegateDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var execManager = new ExecutionManager();
+            var opManager = new OperationManager(20, 0, execManager);
+            opManager.OnChangeDivide += _onChange;
+
+            var result = opManager.Execute(Operation.Divide);
+         
+            Console.WriteLine($"The result of our operation is {result}.");
+        }
+
+        static void _onChange()
+        {
+            Console.WriteLine("Subscription to the event triggered by the publisher trying to divide by zero!");
+        }
+    }
+}
